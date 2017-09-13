b218e4a7e5930bf0d8c9af239a2572c729b15cdb
churris <rene.dev.js@gmail.com>
2017-09-12 23:12:28 -0500

    Create a rollup configuration

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import commonjs from 'rollup-plugin-commonjs';
+import buble from 'rollup-plugin-buble';
+import resolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+import {
+  source,
+  main,
+  module
+} from './package.json';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const filterNulls = (arr) => arr.filter(x => !!x);
+const external = ['react', 'hoist-non-react-statics'];
+const globals = {
+  react: 'React',
+  'hoist-non-react-statics': 'hoistNonReactStatics'
+};
+
+export default [
+  {
+    input: source,
+    output: { file: main, format: 'umd', name: 'statefulDecorator' },
+    external,
+    globals,
+    plugins: filterNulls([
+      resolve(),
+      commonjs(),
+      buble(),
+      isProduction ? uglify() : null
+    ])
+  },
+  {
+    input: source,
+    output: { file: module, format: 'es' },
+    external,
+    globals,
+    plugins: filterNulls([
+      resolve(),
+      commonjs(),
+      buble({
+        target: { chrome: 52 }
+      })
+    ])
+  }
+];
